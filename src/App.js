b1a8d495f2e74c0192c9d958ac75e0a6b130069b
ashish-puttaa <ashishputtaa.98@gmail.com>
2021-01-19 23:12:25 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import 'bulma-scss/bulma.scss';
+
+function App() {
+   return (
+      <div className="container">
+         <div className="section">Hello World</div>
+      </div>
+   );
+}
+
+export default App;
